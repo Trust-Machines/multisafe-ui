880348e9f1d2809c1124d1a3f5cfc858f5b294d9
talhasch <talhabugrabulut@gmail.com>
2022-03-26 14:06:08 +0300

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,10 @@
 import React from 'react';
-import {useAtom} from "jotai";
-import {useState} from "react";
-import {todosAtom, themeAtom} from "./atoms";
-
-function AddTodo() {
-    const [todos, setTodos] = useAtom(todosAtom);
-    const [value, setValue] = useState("");
-
-    return <div style={{padding: "20px"}}>
-        <p>Add Todo</p>
-        <input value={value} onChange={(e) => {
-            setValue(e.target.value)
-        }}/>
-        <button onClick={() => {
-            const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 0;
-            setTodos([...todos, {id, title: value, done: false}])
-        }}>Add
-        </button>
-    </div>;
-}
-
-function TodoList() {
-    const [todos, setTodos] = useAtom(todosAtom);
-    return <>
-        {todos.map(t => {
-            return <div className="todo">
-                <small>id: {t.id} </small>
-                {t.title}
-                {t.done ? <>✅</> : ""}
-                {!t.done && <>
-                    <button onClick={() => {
-                        const newTodos = todos.map(x => {
-                            return {
-                                ...x,
-                                done: x.id === t.id ? true : x.done
-                            }
-                        })
-
-                        setTodos([...newTodos]);
-                    }}>done
-                    </button>
-                </>}
-            </div>
-        })}
-    </>
-}
-
+import Button from '@mui/material/Button';
 
 function App() {
-    const [theme, setTheme] = useAtom(themeAtom);
     return (
         <>
-            <div className="App">
-
-                <p>
-                    Theme: {theme}
-                    <button onClick={() => {
-                        setTheme(theme === "dark" ? "light" : "dark")
-                    }}>Toggle theme
-                    </button>
-                </p>
-                <AddTodo/>
-                <TodoList/>
-            </div>
+            <Button variant="contained">Hello World</Button>
         </>
     );
 }
--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import {Provider} from 'jotai';
 import {useAtomDevtools} from 'jotai/devtools'
-import {themeAtom, todosAtom} from "./atoms";
+import {networkAtom} from "./store";
+
+import {ThemeProvider, createTheme} from "@mui/material";
 
 const AtomsDevtools: React.FC = ({children}) => {
-    useAtomDevtools(todosAtom, 'Lolo')
-    useAtomDevtools(themeAtom, 'Lolo2');
+    useAtomDevtools(networkAtom, 'Network');
 
     return <>{children}</>
 }
 
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 const Providers: React.FC = ({children}) => {
     return (
         <Provider>
-            <AtomsDevtools>{children}</AtomsDevtools>
+            <AtomsDevtools>
+                <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            </AtomsDevtools>
         </Provider>
     )
 }
 
-export default Providers
+export default Providers;
