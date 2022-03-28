ca0ed1a476e7651105052ffb5384252688e4b196
talhasch <talhabugrabulut@gmail.com>
2022-03-28 21:24:37 +0300

    checkpoint

--- src/layout/AppContent/index.tsx
+++ src/layout/AppContent/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Box} from '@mui/material';
 import {grey} from "@mui/material/colors";
+import useAppTheme from "../../hooks/useAppTheme";
 
 const AppContent = (props: { children: React.ReactNode }) => {
+    const [theme] = useAppTheme();
+
     const boxStyles = {
-        bgcolor: grey[100],
+        bgcolor: theme === 'light' ? grey[100] : grey[900],
+        color: theme === 'light' ? grey[900] : grey[300],
         height: 'calc(100% - 40px)',
         flexGrow: 1,
         padding: '20px',
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import {RouteComponentProps} from '@reach/router';
-import {Typography} from '@mui/material';
-
+import {Box, Typography, Button} from '@mui/material';
+import {grey} from '@mui/material/colors';
 import useAuth from '../../hooks/useAuth';
 
 import AppContent from '../../layout/AppContent';
 import AppMenu from '../../layout/AppMenu';
+import {useTheme} from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+const LandingBox = (props: { title: string, subtitle: string, btnLabel: string }) => {
+    const theme = useTheme();
+    const isMd = useMediaQuery(theme.breakpoints.up('md'));
+
+    const [, openAuth, ] = useAuth();
+
+    const boxStyles = {
+        bgcolor: grey[300],
+        padding: '20px',
+        flexShrink: 0,
+        width: isMd ? '260px' : 'auto',
+        marginBottom: isMd ? '0' : '20px'
+    }
+    return <Box sx={boxStyles}>
+        <Typography variant='h6' gutterBottom>{props.title}</Typography>
+        <Typography mb='40px'>{props.subtitle}{' '}</Typography>
+        <Button variant='contained' onClick={openAuth}>{props.btnLabel}</Button>
+    </Box>;
+}
+
+const Landing = () => {
+    const theme = useTheme();
+    const isMd = useMediaQuery(theme.breakpoints.up('md'));
+    const boxStyles = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        maxWidth: isMd ? '640px' : 'auto',
+        flexDirection: isMd ? 'row' : 'column',
+        marginTop: isMd ? '100px' : '40px'
+    }
+    return <>
+        <Typography variant='h4' mt='40px' gutterBottom>Welcome to MultiSafe</Typography>
+        <Typography variant='h6'>MultiSafe is the most secure platform for storing your STX digital assets.</Typography>
+        <Box sx={boxStyles}>
+            <LandingBox title='Create Safe' subtitle='Create a safe with multiple owners' btnLabel='Create Safe'/>
+            <LandingBox title='Load Existing Safe' subtitle='Import a safe to your storage'
+                        btnLabel='Load Existing Safe'/>
+        </Box>
+    </>
+}
 
 const Home = (_: RouteComponentProps) => {
     const [userData, openAuth, signOut] = useAuth();
@@ -13,9 +57,7 @@ const Home = (_: RouteComponentProps) => {
     return <>
         <AppMenu/>
         <AppContent>
-            <Typography variant='h4' gutterBottom>Welcome to MultiSafe</Typography>
-            <Typography variant='h6' mb='40px'>MultiSafe is the most secure platform for storing your STX digital
-                assets.</Typography>
+            {!userData && <Landing/> }
         </AppContent>
     </>
 }
