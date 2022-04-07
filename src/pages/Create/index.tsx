1c8e677d7b0095082a568133441c8bac7bdf42f0
talhasch <talhabugrabulut@gmail.com>
2022-04-07 22:24:18 +0300

    checkpoint

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -4,6 +4,8 @@ import {RouteComponentProps} from '@reach/router';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
+import Stepper from '@mui/material/Stepper';
+
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
@@ -11,7 +13,6 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {validateStacksAddress} from '@stacks/transactions';
 import AppContent from '../../layout/AppContent';
-import AppMenu from '../../layout/AppMenu';
 
 import useUserSession from '../../hooks/useUserSession';
 
@@ -28,10 +29,18 @@ const helpTextStyles = {
 }
 
 const SafeName = (props: { onSubmit: (name: string) => void }) => {
+    const [t] = useTranslation();
+
     return <Box sx={{marginBottom: '30px'}}>
         <Typography
             sx={helpTextStyles}>{'First, let\'s give your new safe a name. Your new safe will be created with this name and it will be stored on blockchain. Therefore, only alphanumeric characters and hyphens accepted.'}</Typography>
         <TextField sx={{width: '100%'}} label={'Safe name'} placeholder={'my-new-safe'} autoFocus={true}/>
+        <BoxFooter>
+            <Button variant="contained" onClick={() => {
+
+                props.onSubmit("ss");
+            }}>{t('Continue')}</Button>
+        </BoxFooter>
     </Box>
 }
 
@@ -39,7 +48,6 @@ const SafeName = (props: { onSubmit: (name: string) => void }) => {
 const SafeOwnerInput = (props: {
     owner: string,
     deletable: boolean,
-    autoFocus: boolean,
     dirty: boolean,
     onChange: (value: string) => void,
     onDelete: () => void,
@@ -55,7 +63,7 @@ const SafeOwnerInput = (props: {
             inputProps={{
                 value: props.owner,
                 label: t('Owner address'),
-                autoFocus: props.autoFocus,
+                autoFocus: true,
                 onChange: (e) => {
                     props.onChange(e.target.value)
                 },
@@ -101,7 +109,7 @@ const SafeOwners = (props: { onBack: () => void, onSubmit: (owners: string[]) =>
             sx={helpTextStyles}>{'Your Safe will have one or more owners. Add additional owners and specify how many of them have to confirm a transaction before it gets executed. In general, the more confirmations required, the more secure your Safe is.'}</Typography>
 
         {owners.map((x, i) => {
-            return <SafeOwnerInput key={i} owner={owners[i]} deletable={i > 0} autoFocus={i > 0} dirty={submitted}
+            return <SafeOwnerInput key={i} owner={owners[i]} deletable={i > 0} dirty={submitted}
                                    hasDuplicate={owners.filter(x => x === owners[i]).length > 1}
                                    onChange={(value) => {
                                        updateOwner(i, value);
@@ -117,28 +125,69 @@ const SafeOwners = (props: { onBack: () => void, onSubmit: (owners: string[]) =>
             <Button sx={{mr: '10px'}} onClick={props.onBack}>{t('Back')}</Button>
             <Button variant="contained" onClick={() => {
                 setSubmitted(true);
-                props.onSubmit(owners);
+                const canSubmit = owners.map(x => validateStacksAddress(x)).filter(x => x).length === owners.length // all addresses are valid
+                    && [...new Set(owners)].length === owners.length // and no duplicates;
+
+                if (canSubmit) {
+                    props.onSubmit(owners);
+                }
             }}>{t('Continue')}</Button>
         </BoxFooter>
     </Box>
 }
 
+
 const Create = (_: RouteComponentProps) => {
     const [, userData, openAuth, signOut] = useUserSession();
     const [t] = useTranslation();
+    const [step, setStep] = useState(0);
+
     return <>
         <AppContent>
             <Typography variant='h4' fontWeight='700' mt='10px' gutterBottom>{t('Create Safe')}</Typography>
             <Typography variant='h6' fontWeight='500'>{t('Create a safe with multiple owners.')}</Typography>
-            <ThemedBox sx={{maxWidth: '640px', mt: '40px', pb: 0, p: '20px'}}>
+            <ThemedBox sx={{maxWidth: '640px', mt: '40px', p: '20px'}}>
+                <Stepper activeStep={step} orientation="vertical">
+                    <Step key={0}>
+                        <StepLabel>Safe Name</StepLabel>
+                        <StepContent>
+                            <SafeName onSubmit={() => {
+                                setStep(step + 1);
+                            }}/>
+                        </StepContent>
+                    </Step>
+                    <Step key={1}>
+                        <StepLabel>Owners</StepLabel>
+                        <StepContent>
+                            <SafeOwners
+                                onBack={() => {
+                                    setStep(step - 1);
+                                }}
+                                onSubmit={() => {
+                                    setStep(step + 1);
+                                }}/>
+                        </StepContent>
+                    </Step>
+                    <Step key={3}>
+                        <StepLabel>Confirmation</StepLabel>
+                        <StepContent>
+                            <SafeOwners
+                                onBack={() => {
+                                    setStep(step - 1);
+                                }}
+                                onSubmit={() => {
+                                    setStep(step + 1);
+                                }}/>
+                        </StepContent>
+                    </Step>
 
-                <SafeOwners onBack={() => {
-                }}
-                            onSubmit={(owners) => {
-                            }}
-                />
+                    <Step key={4}>
+                        <StepLabel>Review</StepLabel>
+                        <StepContent>
 
-
+                        </StepContent>
+                    </Step>
+                </Stepper>
             </ThemedBox>
         </AppContent>
     </>
