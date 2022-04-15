cd48224ef16b07b0d5dc1e63889ad7135e611ddb
talhasch <talhabugrabulut@gmail.com>
2022-04-15 23:41:34 +0300

    recently used safes

--- src/views/home/components/recent.tsx
+++ src/views/home/components/recent.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {useNavigate} from '@reach/router';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import {grey} from '@mui/material/colors';
+import {useTheme} from '@mui/material';
 
 import useMediaBreakPoint from '../../../hooks/use-media-break-point';
 import useTranslation from '../../../hooks/use-translation';
@@ -11,10 +13,11 @@ import ThemedBox from '../../../components/themed-box';
 
 
 const Recent = () => {
-    const [isSm, isMd] = useMediaBreakPoint();
+    const [, isMd] = useMediaBreakPoint();
     const [t] = useTranslation();
     const navigate = useNavigate();
     const [safes,] = useSafes();
+    const theme = useTheme();
 
     if (safes.list.length === 0) {
         return null;
@@ -25,8 +28,23 @@ const Recent = () => {
         width: isMd ? '400px' : null
     }}>
         <Typography variant='h6' gutterBottom>{t('Recently used')}</Typography>
-        <p>qedeqd</p>
-        <p>qedeqd</p>
+        {safes.list.map((i, n) => {
+            const [address, name] = i.split(".");
+            return <Box sx={{
+                cursor: 'pointer',
+                padding: '6px',
+                marginBottom: n === safes.list.length - 1 ? null : '16px',
+                borderRadius: '4px',
+                ':hover': {
+                    bgcolor: theme.palette.mode === 'light' ? grey[50] : grey[900]
+                }
+            }} onClick={() => {
+                navigate(`/safe/${i}`).then();
+            }}>
+                <Box sx={{fontWeight: 'bold'}}>{name}</Box>
+                <Box sx={{fontSize: '90%', overflowWrap: 'break-word'}}>{address}</Box>
+            </Box>
+        })}
     </ThemedBox>
 }
 
