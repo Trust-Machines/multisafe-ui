aa5956f5458dd999fbeba7c5336d58c290a401ce
talhasch <talhabugrabulut@gmail.com>
2022-05-03 13:52:01 +0300

    save

--- src/views/safe/components/dialogs/withdraw-ft.tsx
+++ src/views/safe/components/dialogs/withdraw-ft.tsx
@@ -3,22 +3,83 @@ import DialogTitle from '@mui/material/DialogTitle';
 import CloseModal from '../../../../components/close-modal';
 import DialogContent from '@mui/material/DialogContent';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
-import React from 'react';
+import React, {useMemo, useRef, useState} from 'react';
 import useTranslation from '../../../../hooks/use-translation';
 import useModal from '../../../../hooks/use-modal';
+import CurrencyField from '../../../../components/currency-field';
+import WalletField from '../../../../components/wallet-field';
+import {validateStacksAddress} from '@stacks/transactions';
 
 const WithdrawFt = (props: { asset: FTAsset }) => {
     const [t] = useTranslation();
     const [, showModal] = useModal();
     const {asset} = props;
+    const inputRef = useRef<HTMLInputElement>();
+    const [amount, setAmount] = useState<string>('');
+    const [recipient, setRecipient] = useState<string>('');
 
     const handleClose = () => {
         showModal(null);
     };
 
-    const dialogBody = null;
-    const dialogActions = null;
+    const handleSend = () => {
+
+    }
+
+    const isValid = validateStacksAddress(recipient);
+
+    let errorText = '';
+    if (recipient && !isValid) {
+        errorText = t('Enter a valid Stacks wallet address');
+    }
+
+    const dialogBody = <>
+        <Box sx={{mb: '20px'}}>
+        <CurrencyField
+            isDecimal={asset.decimals > 0}
+            symbol={asset.symbol}
+            onChange={(a) => {
+                setAmount(a);
+            }}
+            fieldProps={{
+                autoFocus: true,
+                inputRef: inputRef,
+                label: t('Enter amount'),
+                value: amount,
+                fullWidth: true,
+                inputProps: {
+                    autoComplete: "off",
+                    autoCorrect: "off",
+                    spellCheck: "false",
+                    maxLength: "20"
+                }
+            }}/>
+        </Box>
+        <WalletField
+            inputProps={{
+                value: recipient,
+                label: t('Recipient'),
+                placeholder: t('Recipient'),
+                autoFocus: true,
+                onChange: (e) => {
+                    setRecipient(e.target.value)
+                },
+                error: !!errorText,
+                helperText: errorText || ' '
+            }}
+            onBnsResolve={(name) => {
+                setRecipient(name);
+            }}
+            isValid={isValid}
+        />
+    </>;
+
+    const dialogActions = <>
+        <Button onClick={handleClose}>{t('Cancel')}</Button>
+        <Button onClick={handleSend}>{t('Send')}</Button>
+    </>;
 
     return (
         <>
