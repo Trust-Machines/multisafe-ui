4c4dd6c699703a19314e84eb6ba7e576f2e4aa8d
talhasch <talhabugrabulut@gmail.com>
2022-05-06 14:58:17 +0300

    save

---
+++ src/constants/nft-list.ts
@@ -0,0 +1,42 @@
+import {NFTAsset} from '../types';
+
+const list: NFTAsset[] = [
+    {
+        address: "SP3D6PV2ACBPEKYJTCMH7HEN02KP87QSP8KTEH335.megapont-ape-club-nft",
+        name: "Megapont Ape Club"
+    },
+    {
+        address: "SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.bitcoin-monkeys",
+        name: "Bitcoin Monkeys"
+    },
+    {
+        address: "SPJW1XE278YMCEYMXB8ZFGJMH8ZVAAEDP2S2PJYG.bitcoin-birds",
+        name: "Bitcoin Birds"
+    },
+    {
+        address: "SPJW1XE278YMCEYMXB8ZFGJMH8ZVAAEDP2S2PJYG.stacks-punks-v3",
+        name: "Stacks Punks V3"
+    },
+    {
+        address: "SP3D6PV2ACBPEKYJTCMH7HEN02KP87QSP8KTEH335.megapont-robot-nft",
+        name: "Megapont Robots"
+    },
+    {
+        address: "SP3QSAJQ4EA8WXEDSRRKMZZ29NH91VZ6C5X88FGZQ.crashpunks-v2",
+        name: "Crash Punks"
+    },
+    {
+        address: "SP2X0TZ59D5SZ8ACQ6YMCHHNR2ZN51Z32E2CJ173.the-explorer-guild",
+        name: "The Explorer Guild"
+    },
+    {
+        address: "SP6P4EJF0VG8V0RB3TQQKJBHDQKEF6NVRD1KZE3C.satoshibles",
+        name: "Satoshibles"
+    },
+    {
+        address: "SP2N3BAG4GBF8NHRPH6AY4YYH1SP6NK5TGCY7RDFA.stacks-mfers",
+        name: "Stacks Mfers"
+    }
+];
+
+export default list;
