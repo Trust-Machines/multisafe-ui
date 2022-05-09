80064ac81abb7cd85e274e07d1f89e79608b3f9f
talhasch <talhabugrabulut@gmail.com>
2022-05-09 14:38:24 +0300

    save

--- src/views/safe/components/dialogs/add-owner.tsx
+++ src/views/safe/components/dialogs/add-owner.tsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
+import DialogContentText from '@mui/material/DialogContentText';
 import {useConnect} from '@stacks/connect-react';
 import {DEPLOYER} from '@trustmachines/multisafe-contracts';
 
@@ -14,6 +15,7 @@ import useSafe from '../../../../hooks/use-safe';
 import useNetwork from '../../../../hooks/use-network';
 import WalletField from '../../../../components/wallet-field';
 import CloseModal from '../../../../components/close-modal';
+import {makeTxUrl} from '../../../../api/helper';
 
 const AddOwner = () => {
     const [safe,] = useSafe();
@@ -24,6 +26,7 @@ const AddOwner = () => {
     const inputRef = useRef<HTMLInputElement>();
     const [owner, setOwner] = useState<string>('');
     const [submitted, setSubmitted] = useState<boolean>(false);
+    const [txId, setTxId] = useState<string>('');
 
     const handleClose = () => {
         showModal(null);
@@ -65,12 +68,12 @@ const AddOwner = () => {
                 noneCV(),
             ],
             onFinish: (data) => {
-                console.log(data)
+                setTxId(data.txId);
             }
         }).then()
     }
 
-    const dialogBody = <>
+    let dialogBody = <>
         <WalletField
             inputProps={{
                 inputRef,
@@ -94,10 +97,27 @@ const AddOwner = () => {
         />
     </>;
 
-    const dialogActions = <>
+    let dialogActions = <>
         <Button onClick={handleClose}>{t('Cancel')}</Button>
         <Button onClick={handleSave}>{t('Add')}</Button>
     </>;
+
+    if (txId) {
+        if (txId) {
+            dialogBody = <DialogContentText component="div">
+                <Box sx={{mb: '12px'}}>{t('A new transaction submitted to add the new owner.')}</Box>
+                <Box sx={{mb: '12px'}}>
+                    {t('It will be available under Transactions section in a few minutes for other owners\' approvals.')}
+                </Box>
+                <Box>
+                    <a href={makeTxUrl(txId, network)} target='_blank' rel='noreferrer'>
+                        {t('View on Blockchain Explorer')}
+                    </a>
+                </Box>
+            </DialogContentText>;
+            dialogActions = <><Button onClick={handleClose}>{t('Close')}</Button></>;
+        }
+    }
 
     return (
         <>
