6f2e73f22194237b722e0643cb7c0d9651fa7eca
talhasch <talhabugrabulut@gmail.com>
2022-05-16 16:39:45 +0300

    save

---
+++ src/hooks/use-safe-call.ts
@@ -0,0 +1,41 @@
+import {FinishedTxData, useConnect} from '@stacks/connect-react';
+import {ClarityValue, contractPrincipalCV, noneCV, someCV, standardPrincipalCV} from '@stacks/transactions';
+import {DEPLOYER} from '@trustmachines/multisafe-contracts';
+import useNetwork from './use-network';
+import useSafe from './use-safe';
+import usePendingTxs from './use-pending-txs';
+
+const useSafeCalls = (): [(owner: string) => Promise<FinishedTxData>] => {
+    const {doContractCall} = useConnect();
+    const [network, stacksNetwork] = useNetwork();
+    const [safe,] = useSafe();
+    const [,syncPendingTxs] = usePendingTxs();
+
+    const doSafeCall = (fn: 'submit' | 'confirm' | 'revoke', args: ClarityValue[]): Promise<FinishedTxData> => new Promise((resolve) => {
+        doContractCall({
+            network: stacksNetwork,
+            contractAddress: safe.address,
+            contractName: safe.name,
+            functionName: fn,
+            functionArgs: args,
+            onFinish: (data) => {
+                resolve(data);
+                syncPendingTxs();
+            }
+        }).then()
+    })
+
+    const safeAddOwnerCall = (owner: string) => doSafeCall('submit', [
+        contractPrincipalCV(DEPLOYER[network], 'add-owner'),
+        contractPrincipalCV(safe.address, safe.name),
+        contractPrincipalCV(DEPLOYER[network], 'ft-none'),
+        contractPrincipalCV(DEPLOYER[network], 'nft-none'),
+        someCV(standardPrincipalCV(owner)),
+        noneCV(),
+        noneCV(),
+    ])
+
+    return [safeAddOwnerCall];
+}
+
+export default useSafeCalls;
--- src/views/safe/components/dialogs/add-owner.tsx
+++ src/views/safe/components/dialogs/add-owner.tsx
@@ -1,32 +1,28 @@
 import React, {useRef, useState} from 'react';
-import {contractPrincipalCV, noneCV, someCV, standardPrincipalCV, validateStacksAddress} from '@stacks/transactions';
+import {validateStacksAddress} from '@stacks/transactions';
 import DialogContent from '@mui/material/DialogContent';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
-import DialogContentText from '@mui/material/DialogContentText';
-import {useConnect} from '@stacks/connect-react';
-import {DEPLOYER} from '@trustmachines/multisafe-contracts';
 
 import useTranslation from '../../../../hooks/use-translation';
 import useModal from '../../../../hooks/use-modal';
 import useSafe from '../../../../hooks/use-safe';
-import useNetwork from '../../../../hooks/use-network';
 import WalletField from '../../../../components/wallet-field';
 import CloseModal from '../../../../components/close-modal';
-import {makeTxUrl} from '../../../../api/helper';
+import useSafeCalls from '../../../../hooks/use-safe-call';
+import CommonTxFeedbackDialog from './common-feedback';
 
 const AddOwner = () => {
     const [safe,] = useSafe();
     const [t] = useTranslation();
     const [, showModal] = useModal();
-    const [network, stacksNetwork] = useNetwork();
-    const {doContractCall} = useConnect();
+
+    const [addOwner,] = useSafeCalls()
     const inputRef = useRef<HTMLInputElement>();
     const [owner, setOwner] = useState<string>('');
     const [submitted, setSubmitted] = useState<boolean>(false);
-    const [txId, setTxId] = useState<string>('');
 
     const handleClose = () => {
         showModal(null);
@@ -50,31 +46,16 @@ const AddOwner = () => {
 
         if (!isValid || error) {
             inputRef.current?.focus();
-
             return;
         }
 
-        doContractCall({
-            network: stacksNetwork,
-            contractAddress: safe.address,
-            contractName: safe.name,
-            functionName: 'submit',
-            functionArgs: [
-                contractPrincipalCV(DEPLOYER[network], 'add-owner'),
-                contractPrincipalCV(safe.address, safe.name),
-                contractPrincipalCV(DEPLOYER[network], 'ft-none'),
-                contractPrincipalCV(DEPLOYER[network], 'nft-none'),
-                someCV(standardPrincipalCV(owner)),
-                noneCV(),
-                noneCV(),
-            ],
-            onFinish: (data) => {
-                setTxId(data.txId);
-            }
-        }).then()
+        addOwner(owner).then((data) => {
+            showModal(<CommonTxFeedbackDialog txId={data.txId} title={t(`Add Owner`)} requiresConfirmation
+                                              description={t('A new transaction submitted to add the new owner.')}/>);
+        });
     }
 
-    let dialogBody = <>
+    const dialogBody = <>
         <WalletField
             inputProps={{
                 inputRef,
@@ -98,25 +79,10 @@ const AddOwner = () => {
         />
     </>;
 
-    let dialogActions = <>
+    const dialogActions = <>
         <Button onClick={handleClose}>{t('Cancel')}</Button>
         <Button onClick={handleSave}>{t('Submit')}</Button>
     </>;
-
-    if (txId) {
-        dialogBody = <DialogContentText component="div">
-            <Box sx={{mb: '12px'}}>{t('A new transaction submitted to add the new owner.')}</Box>
-            <Box sx={{mb: '12px'}}>
-                {t('It will be available under Transactions section in a few minutes for other owners\' approvals.')}
-            </Box>
-            <Box>
-                <a href={makeTxUrl(txId, network)} target='_blank' rel='noreferrer'>
-                    {t('View on Blockchain Explorer')}
-                </a>
-            </Box>
-        </DialogContentText>;
-        dialogActions = <><Button onClick={handleClose}>{t('Close')}</Button></>;
-    }
 
     return (
         <>
