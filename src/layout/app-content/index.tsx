94de4b4a30b73260d2a4e53342b6c04920c6eabe
talhasch <talhabugrabulut@gmail.com>
2022-06-17 13:43:34 +0300

    theme-color meta tag connected to app theme

--- src/layout/app-content/index.tsx
+++ src/layout/app-content/index.tsx
@@ -4,21 +4,23 @@ import {grey} from '@mui/material/colors';
 import {useTheme} from '@mui/material';
 import {SxProps} from '@mui/system';
 import {Theme} from '@mui/material/styles';
+import {Helmet} from 'react-helmet';
 
 const AppContent = (props: { children: React.ReactNode, sx?: SxProps<Theme> }) => {
     const theme = useTheme();
+    const bgcolor = theme.palette.mode === 'light' ? grey[50] : grey[900];
+    const head = <Helmet><meta name="theme-color" content={bgcolor}/></Helmet>;
 
     return <Box sx={{
-        bgcolor: theme.palette.mode === 'light' ? grey[50] : grey[900],
+        bgcolor,
         height: 'calc(100% - 40px)',
         p: '20px',
-        flexDirection:'column',
+        flexDirection: 'column',
         flexGrow: 1,
         display: 'flex',
         position: 'relative',
         ...props.sx
-    }
-    }>{props.children}</Box>
+    }}>{head}{props.children}</Box>
 }
 
 export default AppContent;
