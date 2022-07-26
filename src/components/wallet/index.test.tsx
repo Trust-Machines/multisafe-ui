51cca5bf9ded811a424bee706d9e4f117e80762c
talhasch <talhabugrabulut@gmail.com>
2022-07-26 11:06:55 +0300

    test fixes with renderWithRouter

--- src/components/wallet/index.test.tsx
+++ src/components/wallet/index.test.tsx
@@ -1,16 +1,16 @@
-import {render} from '@testing-library/react';
+import {renderWithRouter} from '../../helper/test-helper';
 import useMediaBreakPoint from '../../hooks/use-media-break-point';
 import Wallet from './index';
 jest.mock('../../hooks/use-media-break-point');
 
 test('1 Default render', () => {
     (useMediaBreakPoint as jest.Mock).mockReturnValue([false, true]);
-    const view = render(<Wallet address="SP3XD84X3PE79SHJAZCDW1V5E9EA8JSKRBPEKAEK7" sx={{background: 'red'}}/>);
+    const view = renderWithRouter(<Wallet address="SP3XD84X3PE79SHJAZCDW1V5E9EA8JSKRBPEKAEK7" sx={{background: 'red'}}/>);
     expect(view.container).toMatchSnapshot()
 });
 
 test('2 Truncate for SM', () => {
     (useMediaBreakPoint as jest.Mock).mockReturnValue([true, false]);
-    const view = render(<Wallet truncateForSm address="SP3XD84X3PE79SHJAZCDW1V5E9EA8JSKRBPEKAEK7"/>);
+    const view = renderWithRouter(<Wallet truncateForSm address="SP3XD84X3PE79SHJAZCDW1V5E9EA8JSKRBPEKAEK7"/>);
     expect(view.container).toMatchSnapshot()
 });
--- src/views/create/components/safe-review.test.tsx
+++ src/views/create/components/safe-review.test.tsx
@@ -1,9 +1,8 @@
-import {render} from '@testing-library/react';
-
 import SafeReview from './safe-review';
+import {renderWithRouter} from '../../../helper/test-helper';
 
 test('1 Default render', () => {
-    const view = render(<SafeReview name="my-safe-2"
+    const view = renderWithRouter(<SafeReview name="my-safe-2"
                                         owners={['SP3XD84X3PE79SHJAZCDW1V5E9EA8JSKRBPEKAEK7', 'SP2DXHX9Q844EBT80DYJXFWXJKCJ5FFAX50CQQAWN']}
                                         confirmations={2}
                                         onBack={() => {}}
