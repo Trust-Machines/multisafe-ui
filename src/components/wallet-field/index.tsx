cd86a04c3373dc5623a2f8f18cfae5c14619c53d
talhasch <talhabugrabulut@gmail.com>
2022-09-12 22:24:09 +0300

    set wallet input label if the address has bns name

--- src/components/wallet-field/index.tsx
+++ src/components/wallet-field/index.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {validateStacksAddress} from 'micro-stacks/crypto';
 
 import TextField from '@mui/material/TextField';
 import {StandardTextFieldProps} from '@mui/material/TextField/TextField';
@@ -6,7 +7,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import CircularProgress from '@mui/material/CircularProgress';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-import {resolveBnsName} from '../../api';
+import {resolveBnsName, getBnsName} from '../../api';
 import useNetwork from '../../hooks/use-network';
 
 interface WalletFieldProps {
@@ -17,16 +18,41 @@ interface WalletFieldProps {
 
 const WalletField = (props: WalletFieldProps) => {
     const [disabled, setDisabled] = useState(false);
+    const [label, setLabel] = useState<string | null>(null);
     const [, stacksNetwork] = useNetwork();
     const adornment = props.isValid ? <CheckCircleIcon color="success"/> : (disabled ?
         <CircularProgress color="primary"/> : null);
 
-    return <TextField {...props.inputProps} fullWidth={true} disabled={disabled} onChange={(e) => {
+    useEffect(() => {
+        const value = props.inputProps.value as string;
+        if (validateStacksAddress(value)) {
+            getBnsName(stacksNetwork, value).then(r => {
+                setLabel(r);
+            })
+        }
+
+    }, [props.inputProps.value, stacksNetwork]);
+
+    const inputProps = {
+        ...props.inputProps,
+        label: label || props.inputProps.label
+    };
+
+    return <TextField {...inputProps} fullWidth={true} disabled={disabled} onChange={(e) => {
         const {value} = e.target;
+
+        if (validateStacksAddress(value)) {
+            getBnsName(stacksNetwork, value).then(r => {
+                setLabel(r);
+            })
+        } else {
+            setLabel(null);
+        }
 
         if (value.endsWith('.btc')) {
             setDisabled(true);
             resolveBnsName(stacksNetwork, value).then(r => {
+                setLabel(value);
                 if (r && props.onBnsResolve) {
                     props?.onBnsResolve(r, value);
                 }
