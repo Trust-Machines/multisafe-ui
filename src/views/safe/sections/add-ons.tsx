f03f4709d5b5fa01b6656e7232a9fcc8e4e0d094
talhasch <talhabugrabulut@gmail.com>
2022-10-05 16:54:57 +0300

    separate add-ons form

--- src/views/safe/sections/add-ons.tsx
+++ src/views/safe/sections/add-ons.tsx
@@ -1,100 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ExtensionIcon from '@mui/icons-material/Extension';
 import Box from '@mui/material/Box';
 import {useNavigate, useParams} from '@reach/router';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import {bufferCVFromString, contractPrincipalCV, noneCV, someCV, uintCV} from 'micro-stacks/clarity';
-import {DEPLOYER} from '@trustmachines/multisafe-contracts';
-import addOns from '../../../constants/add-ons';
-import useSafe from '../../../hooks/use-safe';
-import useSafeCalls from '../../../hooks/use-safe-call';
+
+import AddOnForm from '../components/add-on-form';
 import SectionHeader from '../components/section-header';
-import CommonTxFeedbackDialog from '../components/dialogs/common-feedback';
-import useTranslation from '../../../hooks/use-translation';
 import ThemedBox from '../../../components/themed-box';
+import useSafe from '../../../hooks/use-safe';
+import useTranslation from '../../../hooks/use-translation';
 import useNetwork from '../../../hooks/use-network';
 import useToast from '../../../hooks/use-toast';
-import {AddOn} from '../../../constants/add-ons';
-import useModal from '../../../hooks/use-modal';
-import {principleFromString} from '../../../helper';
-import useAddress from '../../../hooks/use-address';
+import addOns, {AddOn} from '../../../constants/add-ons';
 
-
-const AddOnForm = (props: { readOnly: boolean, addOn: AddOn }) => {
-    const {addOn, readOnly} = props;
-    const {params} = addOn;
-    const [network] = useNetwork();
-    const {safe} = useSafe();
-    const [t] = useTranslation();
-    const [, showMessage] = useToast();
-    const {doSafeCall} = useSafeCalls();
-    const [, showModal] = useModal();
-    const address = useAddress();
-
-    const [paramP,] = useState('');
-    const [paramU, setParamU] = useState('');
-    const [paramB,] = useState('');
-
-    const handleSubmit = () => {
-        if (!address) {
-            showMessage('Login first!', 'error');
-            return;
-        }
-
-        if (readOnly) {
-            showMessage('Only safe owners can submit transactions!', 'error');
-            return;
-        }
-
-        if (params.paramU) {
-            if (parseInt(paramU).toString() !== paramU) {
-                showMessage(`${params.paramU}: Invalid number`, 'error');
-                return;
-            }
-        }
-
-        doSafeCall('submit', [
-            contractPrincipalCV(addOn.executors[network]!.active),
-            contractPrincipalCV(safe.address, safe.name),
-            contractPrincipalCV(DEPLOYER[network], 'ft-none'),
-            contractPrincipalCV(DEPLOYER[network], 'nft-none'),
-            paramP ? someCV(principleFromString(paramP)) : noneCV(),
-            paramU ? someCV(uintCV(paramU)) : noneCV(),
-            paramB ? someCV(bufferCVFromString(paramB)) : noneCV()
-        ]).then(data => {
-            showModal({
-                body: <CommonTxFeedbackDialog
-                    txId={data.txId}
-                    title={addOn.name}
-                    description={t('Your transaction has been submitted.')}
-                    requiresConfirmation={true}
-                />
-            });
-        })
-    }
-
-    return <>
-        <Box sx={{maxWidth: '500px'}}>
-            {params.paramU && (<Box sx={{mb: '20px'}}>
-                    <TextField fullWidth
-                               label={params.paramU}
-                               value={paramU}
-                               inputProps={{
-                                   type: 'number',
-                                   step: '1',
-                                   pattern: '^[-\\d]\\d*$'
-                               }}
-                               onChange={(e) => {
-                                   setParamU(e.target.value);
-                               }}
-                    />
-                </Box>
-            )}
-            <Button variant="contained" onClick={handleSubmit}>Submit</Button>
-        </Box>
-    </>
-}
 
 const AddOnsList = (props: { readOnly: boolean }) => {
     const [t] = useTranslation();
