50aa26eeb474c7ca2f47f6fff58122d861460c55
talhasch <talhabugrabulut@gmail.com>
2023-10-11 17:46:01 +0300

    fix devtools error

--- src/providers/dev-tools.tsx
+++ src/providers/dev-tools.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import {useAtomDevtools} from 'jotai/devtools';
-import {networkAtom} from '../store';
+
 
 const DevToolsProvider: React.FC = ({children}) => {
-    useAtomDevtools(networkAtom, 'Network');
+
     return <>{children}</>;
 }
 
